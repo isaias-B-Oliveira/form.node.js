b07d49fac07ffe4846b426f9e820d6018fe3cb76
ISAIAS <126277721+isaias-B-Oliveira@users.noreply.github.com>
2023-07-19 14:11:58 -0300

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 // //conectando com o mysql: npm i --save sequelize && npm i --save mysql2
 const Sequelize = require('sequelize');
-
+//config sequelize
 const sequelize = new Sequelize('formulario', 'root', 'sua senha mysql', {
     host: "localhost",
     dialect:'mysql'
